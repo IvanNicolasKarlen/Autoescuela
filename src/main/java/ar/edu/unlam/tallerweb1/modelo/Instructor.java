c1acdf5ac43386f2540dc85974ecc51551aa9a30
DianaAguayo <31738298+DianaAguayoUnlam@users.noreply.github.com>
2019-09-09 16:25:37 -0300

    elimino nombre y apellido

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Instructor.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Instructor.java
@@ -11,9 +11,6 @@ public class Instructor {
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String nombre;
-	private String apellido;
-	
 	
 	@OneToOne
 	private Usuario usuario; 
@@ -25,18 +22,8 @@ public class Instructor {
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getApellido() {
-		return apellido;
-	}
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
+	
+	
 	public Usuario getUsuario() {
 		return usuario;
 	}
