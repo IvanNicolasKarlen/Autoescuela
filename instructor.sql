1932b422c2466cbda07590666dc3ebdf04bce8e5
julieta <julieta.iracheta@hotmail.com>
2019-11-06 15:42:15 -0300

    actualizado

--- instructor.sql
+++ instructor.sql
@@ -1,4 +1,5 @@
 create schema tallerwebtpfinal;
+
 use tallerwebtpfinal;
 
 INSERT INTO instructor (id) values (1);
@@ -7,7 +8,9 @@ INSERT INTO usuario (apellido,dni,email,nombre,nombreDeUsuario,password,rol,inst
 VALUES('Pardo', 12345678, 'pardo@hotmail.com','Sebastian','SebastianPardo','123','Instructor',1);
 
 insert into estadoDeVehiculo(estadoActual)
-VALUES	("Averiado");
+VALUES	("Disponible"),
+        ("Cancelado por el instructor"),
+        ("Vehiculo Averiado");
 
 insert into especialidad(tipo)
 VALUES	("Moto"),("Auto");
@@ -21,6 +24,9 @@ VALUES	("XYZ-123","Honda 2003",2,1),
 		("FGH-978","Mercedes Benz 4x4",1,1),
         ("GRF-987","Scooter x32",2,1);
 
+INSERT INTO estadodevehiculo_vehiculo (estadodevehiculo_id, vehiculos_id) values (1,1),
+                                                                                 (1,2),
+                                                                                 (1,3);
 
 INSERT INTO instructorvehiculoespecialidad (especialidad_id,instructor_id,vehiculo_id)  values (1,1,1);
 
@@ -45,22 +51,29 @@ INSERT INTO alumno_inscripcion (Alumno_id,inscripciones_id) values (5,5);
 
 INSERT INTO estadoDeAgenda (estado) values ("Disponible"),
                                            ("Cancelado por el instructor"),
-                                           ("Vehiculo averiado");
+                                           ("Vehiculo Averiado");
 
 INSERT INTO agenda (fecha,hora,estadoDeAgenda_id,inscripcion_id,instructorVehiculoEspecialidad_id) 
-                                                                                 values ('2019-02-02',1200,2,2,1),
+                                                                                 values ('2019-02-02',1200,1,2,1),
                                                                                         ('2019-07-02',1300,1,3,1),
                                                                                         ('2019-07-03',1600,1,4,1),
+                                                                                        ('2019-08-08',1800,1,2,1),
                                                                                         ('2019-07-02',1400,1,5,1);
-/*INSERT INTO inscripcion_agenda (Inscripcion_id,agendas_id) values (1,1);
-INSERT INTO inscripcion_agenda (Inscripcion_id,agendas_id) values (2,2);*/
+INSERT INTO estadodeagenda_agenda (estadodeagenda_id,agendas_id) values (2,1),
+                                                                        (1,2),
+                                                                        (1,3),
+                                                                        (1,4);
 
 INSERT INTO usuario (apellido,dni,email,nombre,nombreDeUsuario,password,rol,alumno_id)
-       VALUES('Iracheta', 39625638, 'juli@hotmail.com','Julieta','JulietaIracheta','123','Alumno',2),
+       VALUES('Iracheta', 39654789, 'lucas@hotmail.com','Lucas','LucasIracheta','123','Alumno',2),
              ('Iracheta', 11111111, 'daysi@hotmail.com','Daysi','DaysiIracheta','123','Alumno',3),
              ('Acosta', 39055655, 'alan@hotmail.com','Alan','AlanAcosta','123','Alumno',4),
              ('Iracheta', 39625638, 'julii@hotmail.com','Julieta','JulietaIracheta','123','Alumno',5);
 /* ---------------------------------------------------------------------------------------------*/
+         UPDATE alumno 
+       set usuario_id = 1
+       where id = 1;
+
        UPDATE alumno 
        set usuario_id = 2
        where id = 2;
